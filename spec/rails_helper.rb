cd3f29887aa15f39e5525f39d599362f90befe34
Chris Todorov <chris@super.gd>
2022-01-25 18:16:54 -0800

    Include Solidus factories and FactoryBot syntax methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
+require_relative "../config/environment"
+
+require 'spree/testing_support/factory_bot'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -30,6 +35,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Spree::TestingSupport::FactoryBot.add_paths_and_load!
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -61,4 +69,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
 end
